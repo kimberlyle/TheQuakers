c861b0f5bbd93fd91ffc0c54bb01e334bd40869e
sunnymh <sunnymh2009@gmail.com>
2013-11-28 01:05:04 -0800

    Done with ErrorRateFunction, test it on MDA Model. Will update MDAModelTest with comments later

--- skeleton/ErrorRateFunction.R
+++ skeleton/ErrorRateFunction.R
@@ -1,68 +1,54 @@
-data = read.csv("~/stat157/TheQuakers/skeleton/DataFrame.csv")
-names(data)
-data1 = data[data$magnitude>=3,]
-data2 = data[data$magnitude>=3.5,]
-data3 = data[data$magnitude>4,]
-data4 = data[data$magnitude>3,]
-
-# k * nu ^M
-
-data = data4
-window_size = rep(2, nrow(data4))
-
-get_error_rate = function(data, window_size){
-  size = nrow(data)
-  left_alarm = data$time
-  right_alarm = data$time + window_size
-  right_alarm[right_alarm>max(data$time)] = max(data$time)
-  left_all = rep(0, size)
-  right_all = rep(0, size)  
-  left_curr = 0
-  right_curr = 0
-  predicted = rep(FALSE, size)  
+# Input:
+# time: time for each earthquake in days
+# window_size: length of alarm for each corresponding earthquake in days
+# output: a list with element 'v' and 'tau'
+# v: the error rate for the input
+# tau: time covered by alarm / (time[last] - time[first]) 
+get_error_rate = function(time, window_size){
+  # check for input
+  if (!is.numeric(time) | !is.numeric(window_size)) stop('time and window_size must be all numeric')
+  if (length(time) != length(window_size)) stop('time and window_size should have same length')
+  if (any(is.na(time)) | any(is.na(window_size))) stop('time and window_size shoud not contain NAs')
+  
+  # Get information about the input
+  size = length(time)  
+  right_alarm = time + window_size
+  right_alarm[right_alarm>max(time)] = max(time) # time won't go beyond the last earthquake
+  
+  # Initialize variables
+  actual_left = rep(0, size)
+  actual_right = rep(0, size)  
+  right_max_so_far = 0
+  predicted = rep(FALSE, size)
+  
+  # Get actual alarms for each earthquake
   for (i in 1:size){
-    if (left_alarm[i] > right_curr){
-      left_all[i] = left_curr = left_alarm[i]
-      right_all[i] = right_curr = right_alarm[i]
-    } else if (right_alarm[i] > right_curr){
-      left_all[i] = left_curr = right_curr
-      right_all[i] = right_curr = right_alarm[i]
+    # Case two earthquakes occurs at the same time (not likely)
+    if ((i != 1) && (time[i] == time[i-1])){
+      if (right_alarm[i] > right_max_so_far){
+        actual_left[i] = right_max_so_far
+        actual_right[i] = right_max_so_far = right_alarm[i]
+      }
+      predicted[i] = predicted[i-1]
+    # Case the earthquake is not predicted  
+    } else if (time[i] > right_max_so_far){
+      actual_left[i] = time[i]
+      actual_right[i] = right_max_so_far = right_alarm[i]
+    # Case the earthquake is predicted and its alarm is longer than the existing one
+    } else if (right_alarm[i] >right_max_so_far){
+      actual_left[i] = right_max_so_far
+      actual_right[i] = right_max_so_far = right_alarm[i]
       predicted[i] = TRUE
+    # Case the earthquake is predicted and its alarm is shorter than the existing one
     } else {
       predicted[i] = TRUE
     }
   }
+  
+  # Get v and tau
   v = 1 - sum(predicted) / size
-  tau = sum(right_all - left_all)/(max(data$time) - min(data$time))
+  tau = sum(actual_right - actual_left)/(max(time) - min(time))
+  
+  # Return the list
   return (list('v' = v, 'tau' = tau))
 }
-
-taus = rep(0, 100)
-vs = rep(0, 100)
-
-
-for (i in 1:100){
-  window_size = rep(i, nrow(data4))
-  result = get_error_rate(data4, window_size)
-  taus[i] = result$tau
-  vs[i] = result$v  
-}
-plot(taus, vs, cex = 0.5, pch = 19)
-
-
-for (i in 1:100){
-  window_size = i * 1.5^data4$magnitude
-  result = get_error_rate(data4, window_size)
-  taus[i] = result$tau
-  vs[i] = result$v  
-}
-plot(taus, vs, cex = 0.5, pch = 19, type = 'l')
-
-
-for (i in 1:100){
-  window_size = 0.05 * i*(1.1)^data4$magnitude
-  result = get_error_rate(data4, window_size)
-  taus[i] = result$tau
-  vs[i] = result$v  
-}
-plot(taus, vs, cex = 0.5, pch = 19)
