a4ef3c60d59cf74e13df99ef8ecf032be49df2ae
tandrasfay <tandrasfay@gmail.com>
2013-12-05 10:33:13 -0800

    Add startyear

--- Resources/ExploratoryDataAnalysis.R
+++ Resources/ExploratoryDataAnalysis.R
@@ -1,16 +1,16 @@
 DataFrame<-read.csv("Insert File Pathway Here")
 head(DataFrame)
 
-DataFrame = DataFrame[DataFrame$year >=1990,]
+startyear<-1990  ### Change starting year here
+DataFrame = DataFrame[DataFrame$year >=startyear,]
 head(DataFrame)
 head(DataFrame$magnitude,100)
 
-Year1990plus<-DataFrame[DataFrame$year>=1990,]
-head(Year1990plus)
-Mag3plus1990present<-Year1990plus[which(Year1990plus$magnitude>=3),]
-Mag3plus1990present$inter<-rep(0,(length(Mag3plus1990present$time)))
-for(i in 1:length(Mag3plus1990present)){
-  Mag3plus1990present$inter[i]<-Mag3plus1990present$time[i+1]-Mag3plus1990present$time[i]
+
+Mag3plus<-DataFrame[which(DataFrame$magnitude>=3),]
+Mag3plus$inter<-rep(0,(length(Mag3plus$time)))
+for(i in 1:nrow(Mag3plus)){
+  Mag3plus$inter[i]<-Mag3plus$time[i+1]-Mag3plus$time[i]
 }
-head(Mag3plus1990present)
-plot(Mag3plus1990present$magnitude,Mag3plus1990present$inter)
+head(Mag3plus)
+plot(Mag3plus$magnitude,Mag3plus$inter)
